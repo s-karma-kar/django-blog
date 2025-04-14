0dce15b8baeb741ef7ca0f6d83777f542779bb2a
shylpee <143755066+s-karma-kar@users.noreply.github.com>
2025-04-13 17:27:22 -0700

    Fix: enforce black formatting on urls.pytry2

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,10 +1,10 @@
 # mysite/urls.py
 from django.contrib import admin
-from django.urls import path, include  # <-- Make sure you have both of these imports.
 from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import include, path
 
 urlpatterns = [
-    path("polling/", include("polling.urls")),  # <-- Add this
+    path("polling/", include("polling.urls")),
     path("admin/", admin.site.urls),
     path("", include("blogging.urls")),
     path("login/", LoginView.as_view(template_name="login.html"), name="login"),
